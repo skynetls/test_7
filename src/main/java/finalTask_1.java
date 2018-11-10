6dabab85b94cc48987e45ffb29a3c756945f5cdd
skynetls <lyudocheksi@gmail.com>
2018-11-11 02:10:21 +0300

    Initial commit

---
+++ src/main/java/finalTask_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class finalTask_1 {
+    public static void main(String[] args) {
+        int multiplier1[] = {1,2,3,4,5,6,7,8,9};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Вывести таблицу умножения на ");
+        int multiplier2 = scanner.nextInt();
+
+        for (int i = 0; i < multiplier1.length; i++){
+            System.out.println(multiplier1[i] + "*" + multiplier2 + "=" + multiplier1[i]*multiplier2);
+        }
+
+    }
+}
